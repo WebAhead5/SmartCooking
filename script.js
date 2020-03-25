d758543d4ed8d8b197ac07a2179c475d5aec1bb9
Hashem Abed Al Jawad <dashinghashem@yahoo.com>
2020-03-25 12:20:12 +0200

    create options

--- script.js
+++ script.js
@@ -9,8 +9,19 @@ fetch(url)
     })
 
     .then(function (data) {
-        for(let i=0 ;i <data.results.length;i++)
+        for(let i=0 ;i <data.results.length;i++){
     RecipeMang.push({href:data.results[i].href,ingredients:data.results[i].ingredients,thumbnail:data.results[i].thumbnail,title:data.results[i].title});
+    var option =document.createElement("OPTION");
+    var Recipe = document.createTextNode(RecipeMang[i].title);
+    option.appendChild(Recipe);
+    document.getElementById("Recipes").appendChild(option);
+    //
+        }
     });
+
+    console.log(RecipeMang);
+
+
+
 
 
