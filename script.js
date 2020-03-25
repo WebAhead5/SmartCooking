e44e2ef51e4cace07a50d21375cc8a98a97567d6
Hashem Abed Al Jawad <dashinghashem@yahoo.com>
2020-03-25 21:35:55 +0200

    Add Search Bar

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var RecipeMang =[]; 
-	
+var tempRec=[];
+
 const url ="https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/ ";
 
 fetch(url)
@@ -22,11 +23,14 @@ fetch(url)
 
     });
 
-
-
      
     function getref()
     {
+    var foodbyvalue=document.getElementById("Ingerfood").value
+
+    // Enter Here if the user doesn't have an input for the Ingredients
+
+        if (foodbyvalue=="") {
         var e = document.getElementById("Recipes");
         var strUser = e.options[e.selectedIndex].value;
         var Index1 = RecipeMang.findIndex(checkIndex) 
@@ -34,14 +38,31 @@ fetch(url)
         return RecipeMang.title == strUser;}
         var href=(RecipeMang[Index1].href);
         var win = window.open(href, '_blank');
+    }
+
+    // If the user type an ingredient , this will be executed
+
+    if (foodbyvalue!="") {
+
+        var e2 = document.getElementById("Recipes");
+        var strUser2 = e2.options[e2.selectedIndex].value;
+        var Index2 = tempRec.findIndex(checkIndex2) 
+        function checkIndex2(tempRec) {
+        return tempRec.title == strUser2;}
+        var href2=(tempRec[Index2].href);
+        var win2 = window.open(href2, '_blank');
+    }
 
     }
 
     
-
     function get_ingredient()
     {
-        clearBox('ingredient');
+    var foodbyvalue=document.getElementById("Ingerfood").value
+    clearBox('ingredient');
+
+    if (foodbyvalue=="") {
+        
 
         var my_list =document.createElement("ul");
         var elem = document.createElement("li");
@@ -61,9 +82,66 @@ fetch(url)
 
 
         document.getElementById("Picture").src=RecipeMang[Index1].thumbnail;
+        document.getElementById("Picture").style.cssText="border: red dashed";
+        
 } 
+
+if (foodbyvalue!="") {
+
+    var my_list2 =document.createElement("ul");
+        var elem2 = document.createElement("li");
+
+
+        var e2 = document.getElementById("Recipes");
+        var strUser2 = e2.options[e2.selectedIndex].value;
+        var Index2 = tempRec.findIndex(checkIndex2) 
+        function checkIndex2(tempRec) {
+        return tempRec.title == strUser2;}
+
+
+
+        elem2.textContent=tempRec[Index2].ingredients;
+        my_list2.appendChild(elem2);
+        document.getElementById("ingredient").appendChild(my_list2);
+
+
+        document.getElementById("Picture").src=tempRec[Index2].thumbnail;
+        document.getElementById("Picture").style.cssText="border: red dashed";
+
+
+    }
+
+}
+
 
     function clearBox(elementID)
 {
     document.getElementById(elementID).innerHTML = "";
+    document.getElementById(elementID).value = "";
+}
+
+
+function IngFunction()
+{
+
+    var url2="https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?i=";
+    var foodbyInger=document.getElementById("Ingerfood").value;
+    url2+=foodbyInger;
+
+
+    fetch(url2)
+    .then(function (response) {
+        return response.json();
+    })
+
+    .then(function (data2) {
+        for(let i=0 ;i <data2.results.length;i++){
+            tempRec.push({href:data2.results[i].href,ingredients:data2.results[i].ingredients,thumbnail:data2.results[i].thumbnail,title:data2.results[i].title});   
+            var option =document.createElement("OPTION");
+            var Recipe = document.createTextNode(tempRec[i].title);
+            option.appendChild(Recipe);
+            document.getElementById("Recipes").appendChild(option);
+    }
+
+}); 
 }
