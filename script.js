7553cc88d7fdcb7183481b060ad472a1e8e8fec6
Hashem Abed Al Jawad <dashinghashem@yahoo.com>
2020-03-25 16:37:46 +0200

    Giving Pictures

--- script.js
+++ script.js
@@ -11,33 +11,59 @@ fetch(url)
     .then(function (data) {
         for(let i=0 ;i <data.results.length;i++){
     RecipeMang.push({href:data.results[i].href,ingredients:data.results[i].ingredients,thumbnail:data.results[i].thumbnail,title:data.results[i].title});
+    
     var option =document.createElement("OPTION");
     var Recipe = document.createTextNode(RecipeMang[i].title);
     option.appendChild(Recipe);
     document.getElementById("Recipes").appendChild(option);
-    //
-    var my_list =document.createElement("ul");
-    var elem = document.createElement("li");
-    elem.textContent=RecipeMang[Index1].ingredients;
-    my_list.appendChild(elem);
-    document.getElementById("ingredient").appendChild(my_list);
+
+    }
 
 
-        }
     });
 
-      
+
+
+     
     function getref()
     {
-
         var e = document.getElementById("Recipes");
         var strUser = e.options[e.selectedIndex].value;
-       // console.log(strUser);
         var Index1 = RecipeMang.findIndex(checkIndex) 
         function checkIndex(RecipeMang) {
         return RecipeMang.title == strUser;}
         var href=(RecipeMang[Index1].href);
         var win = window.open(href, '_blank');
 
-
     }
+
+    
+
+    function get_ingredient()
+    {
+        clearBox('ingredient');
+
+        var my_list =document.createElement("ul");
+        var elem = document.createElement("li");
+
+
+        var e = document.getElementById("Recipes");
+        var strUser = e.options[e.selectedIndex].value;
+        var Index1 = RecipeMang.findIndex(checkIndex) 
+        function checkIndex(RecipeMang) {
+        return RecipeMang.title == strUser;}
+
+
+
+        elem.textContent=RecipeMang[Index1].ingredients;
+        my_list.appendChild(elem);
+        document.getElementById("ingredient").appendChild(my_list);
+
+
+        document.getElementById("Picture").src=RecipeMang[Index1].thumbnail;
+} 
+
+    function clearBox(elementID)
+{
+    document.getElementById(elementID).innerHTML = "";
+}
