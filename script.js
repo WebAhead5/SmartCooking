73633818655f941cd011b3676b99593bc7c5adfd
amirfahoum <amirfahoum@gmail.com>
2020-03-25 10:35:18 +0200

    add database

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+var RecipeMang =[]; 
+	
+const url ="https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/ ";
+
+fetch(url)
+
+    .then(function (response) {
+        return response.json();
+    })
+
+    .then(function (data) {
+        for(let i=0 ;i <data.results.length;i++)
+    RecipeMang.push({href:data.results[i].href,ingredients:data.results[i].ingredients,thumbnail:data.results[i].thumbnail,title:data.results[i].title});
+    });
+    console.log(RecipeMang);
+
+
+
