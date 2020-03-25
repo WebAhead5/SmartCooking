4e7473e6c09cef59edb6b965b8fae23c999fc572
amirfahoum <amirfahoum@gmail.com>
2020-03-25 12:21:32 +0200

    get database+design

--- script.js
+++ script.js
@@ -10,7 +10,20 @@ fetch(url)
 
     .then(function (data) {
         for(let i=0 ;i <data.results.length;i++)
-    RecipeMang.push({href:data.results[i].href,ingredients:data.results[i].ingredients,thumbnail:data.results[i].thumbnail,title:data.results[i].title});
-    });
+      RecipeMang.push({href:data.results[i].href,ingredients:data.results[i].ingredients,thumbnail:data.results[i].thumbnail,title:data.results[i].title});
+    })
 
+    function getref() {
+        var a = document.createElement('a');
+        var linkText = document.createTextNode("my title text");
+        a.appendChild(linkText);
+        a.href = data[i].ref;
+        document.body.appendChild(a);
+        console.log(a.href);
+         document
+
+        
+
+    };
+    
 
