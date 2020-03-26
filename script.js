f607db75f6a32d9b836ba6669929350d5fe0b169
Hashem Abed Al Jawad <dashinghashem@yahoo.com>
2020-03-26 13:43:56 +0200

    Final Touch

--- script.js
+++ script.js
@@ -1,147 +1,157 @@
-var RecipeMang =[]; 
-var tempRec=[];
+var recipiesList = [];
+var recByIngredient = [];
 
-const url ="https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/ ";
+// Fetching from the main page
 
+url = "https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/";
 fetch(url)
 
     .then(function (response) {
         return response.json();
     })
 
-    .then(function (data) {
-        for(let i=0 ;i <data.results.length;i++){
-    RecipeMang.push({href:data.results[i].href,ingredients:data.results[i].ingredients,thumbnail:data.results[i].thumbnail,title:data.results[i].title});
-    
-    var option =document.createElement("OPTION");
-    var Recipe = document.createTextNode(RecipeMang[i].title);
-    option.appendChild(Recipe);
-    document.getElementById("Recipes").appendChild(option);
+    .then(function set_data(data) {
+        for (let i = 0; i < data.results.length; i++) {
+            recipiesList.push({ href: data.results[i].href, ingredients: data.results[i].ingredients, thumbnail: data.results[i].thumbnail, title: data.results[i].title });
 
-    }
+            var option = document.createElement("OPTION");
+            var Recipe = document.createTextNode(recipiesList[i].title);
+            option.appendChild(Recipe);
+            document.getElementById("Recipes").appendChild(option);
 
-
+        }
     });
+    
+    // Fetching from page with the Ingredient that the user entered
 
-     
-    function getref()
-    {
-    var foodbyvalue=document.getElementById("Ingerfood").value
+    function IngFetch() {
+        var foodbyInger = document.getElementById("Ingerfood").value;
+        clearBox("Ingerfood");
+        url2 = url+"?i=" + foodbyInger;
+    
+        fetch(url2)
+            .then(function (response1) {
+                return response1.json();
+            })
+    
+            .then(function set_data2(data) {
+                clearBox("Recipes");
+                for (let i = 0; i < data.results.length; i++) {
+                    recByIngredient.push({ href: data.results[i].href, ingredients: data.results[i].ingredients, thumbnail: data.results[i].thumbnail, title: data.results[i].title });
+    
+                    var option1 = document.createElement("OPTION");
+                    var Recipe2 = document.createTextNode(recByIngredient[i].title);
+                    option1.appendChild(Recipe2);
+                    document.getElementById("Recipes").appendChild(option1);
+    
+                }
+    
+            });
+        recByIngredient = [];
+    
+    }
+    
 
-    // Enter Here if the user doesn't have an input for the Ingredients
+// Checking if the user has an input for the Ingredient and afterwards taking the data either from the main page
+// or using the data from the page that user is searching for.
 
-        if (foodbyvalue=="") {
+function getref() {
+    var inputIngredient = document.getElementById("Ingerfood").value
+
+    // Here we are taking all the recipes titles that we already put in the select section where the user can choose
+    // one of them.Afterwards, we are checking the user selection with the list of recipies that we have and from
+    // that we get the index of the recipe which is has href as on of it's keys and from that we can access to the 
+    // link which stands for each single recipe.
+
+    if (inputIngredient == "") {
         var e = document.getElementById("Recipes");
         var strUser = e.options[e.selectedIndex].value;
-        var Index1 = RecipeMang.findIndex(checkIndex) 
-        function checkIndex(RecipeMang) {
-        return RecipeMang.title == strUser;}
-        var href=(RecipeMang[Index1].href);
+        var Index1 = recipiesList.findIndex(checkIndex)
+        function checkIndex(recipiesList) {
+            return recipiesList.title == strUser;
+        }
+        var href = (recipiesList[Index1].href);
         var win = window.open(href, '_blank');
     }
 
     // If the user type an ingredient , this will be executed
 
-    if (foodbyvalue!="") {
+    if (inputIngredient != "") {
 
         var e2 = document.getElementById("Recipes");
         var strUser2 = e2.options[e2.selectedIndex].value;
-        var Index2 = tempRec.findIndex(checkIndex2) 
-        function checkIndex2(tempRec) {
-        return tempRec.title == strUser2;}
-        var href2=(tempRec[Index2].href);
+        var Index2 = recByIngredient.findIndex(checkIndex2)
+        function checkIndex2(recByIngredient) {
+            return recByIngredient.title == strUser2;
+        }
+        var href2 = (recByIngredient[Index2].href);
         var win2 = window.open(href2, '_blank');
     }
 
-    }
+}
 
-    
-    function get_ingredient()
-    {
-    var foodbyvalue=document.getElementById("Ingerfood").value
+
+// After deciding which link to fetch depending on the user typing an Ingredient or not.The next step will be showing
+// the Ingredients of each recipe on the right side of the screen with updaing the picture in the middle which stands
+// for each recipe
+
+
+function get_ingredient() {
+    var inputIngredient = document.getElementById("Ingerfood").value
     clearBox('ingredient');
 
-    if (foodbyvalue=="") {
-        
+    if (inputIngredient == "") {
 
-        var my_list =document.createElement("ul");
+
+        var my_list = document.createElement("ul");
         var elem = document.createElement("li");
 
 
         var e = document.getElementById("Recipes");
         var strUser = e.options[e.selectedIndex].value;
-        var Index1 = RecipeMang.findIndex(checkIndex) 
-        function checkIndex(RecipeMang) {
-        return RecipeMang.title == strUser;}
+        var Index1 = recipiesList.findIndex(checkIndex)
+        function checkIndex(recipiesList) {
+            return recipiesList.title == strUser;
+        }
 
-
-
-        elem.textContent=RecipeMang[Index1].ingredients;
+        elem.textContent = recipiesList[Index1].ingredients;
         my_list.appendChild(elem);
         document.getElementById("ingredient").appendChild(my_list);
 
+        document.getElementById("Picture").src = recipiesList[Index1].thumbnail;
+        document.getElementById("Picture").style.cssText = "border: red dashed";
 
-        document.getElementById("Picture").src=RecipeMang[Index1].thumbnail;
-        document.getElementById("Picture").style.cssText="border: red dashed";
-        
-} 
+    }
 
-if (foodbyvalue!="") {
+    if (inputIngredient != "") {
 
-    var my_list2 =document.createElement("ul");
+        var my_list2 = document.createElement("ul");
         var elem2 = document.createElement("li");
 
 
         var e2 = document.getElementById("Recipes");
         var strUser2 = e2.options[e2.selectedIndex].value;
-        var Index2 = tempRec.findIndex(checkIndex2) 
-        function checkIndex2(tempRec) {
-        return tempRec.title == strUser2;}
+        var Index2 = recByIngredient.findIndex(checkIndex2)
+        function checkIndex2(recByIngredient) {
+            return recByIngredient.title == strUser2;
+        }
 
 
 
-        elem2.textContent=tempRec[Index2].ingredients;
+        elem2.textContent = recByIngredient[Index2].ingredients;
         my_list2.appendChild(elem2);
         document.getElementById("ingredient").appendChild(my_list2);
 
 
-        document.getElementById("Picture").src=tempRec[Index2].thumbnail;
-        document.getElementById("Picture").style.cssText="border: red dashed";
+        document.getElementById("Picture").src = recByIngredient[Index2].thumbnail;
+        document.getElementById("Picture").style.cssText = "border: red dashed";
 
 
     }
 
 }
 
+function clearBox(elementID) {
 
-    function clearBox(elementID)
-{
     document.getElementById(elementID).innerHTML = "";
-    document.getElementById(elementID).value = "";
-}
-
-
-function IngFunction()
-{
-
-    var url2="https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?i=";
-    var foodbyInger=document.getElementById("Ingerfood").value;
-    url2+=foodbyInger;
-
-
-    fetch(url2)
-    .then(function (response) {
-        return response.json();
-    })
-
-    .then(function (data2) {
-        for(let i=0 ;i <data2.results.length;i++){
-            tempRec.push({href:data2.results[i].href,ingredients:data2.results[i].ingredients,thumbnail:data2.results[i].thumbnail,title:data2.results[i].title});   
-            var option =document.createElement("OPTION");
-            var Recipe = document.createTextNode(tempRec[i].title);
-            option.appendChild(Recipe);
-            document.getElementById("Recipes").appendChild(option);
-    }
-
-}); 
 }
